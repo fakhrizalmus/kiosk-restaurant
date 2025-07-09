77dfbc1fd82d458c241f9623719f1f3c840e77ca
Fakhrizal Mustaqim <fakhrizaluciha@gmail.com>
2025-07-09 15:45:59 +0700

    fix backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,22 @@
-import express from 'express'
+require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bp = require("body-parser");
+const PORT = process.env.PORT
+const router = require("./routes/index")
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', router, express.static('public'))
+
+app.get("/", (req, res) => {
+  res.send("halo");
+  console.log("test");
+});
+
+app.listen(PORT, () => {
+  console.log(`dengar di ${PORT}`);
+});
---
+++ backend/migrations/20250709084306-create-transaction.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      cart_id: {
+        type: Sequelize.INTEGER
+      },
+      total: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      paid_at: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Transactions');
+  }
+};
