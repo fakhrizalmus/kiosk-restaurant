099c5b9641ab5d5877f3185182c9989bdb92d6e7
Fakhrizal Mustaqim <135151144+fakhrizalmus@users.noreply.github.com>
2025-11-28 09:53:06 +0700

    Update page.tsx

--- frontend/src/app/(admin)/role/page.tsx
+++ frontend/src/app/(admin)/role/page.tsx
@@ -4,36 +4,36 @@ import { useEffect, useState } from "react"
 import { DataTable } from "./data-table"
 import { CardTitle } from "@/components/ui/card"
 import AddModal from "./addmodal"
-import { deletePermission, getPermission } from "../actions"
+import { deleteRole, getRole } from "../actions"
 
 export default function Page() {
-  const [permission, setPermission] = useState<any[]>([]);
-  const [countPermission, setCountPermission] = useState<number>(0);
+  const [role, setRole] = useState<any[]>([]);
+  const [countRole, setCountRole] = useState<number>(0);
   const [pageSize, setPageSize] = useState<number>(10)
   const [pageIndex, setPageIndex] = useState<number>(0)
 
-  const fetchPermission = async () => {
-    const res = await getPermission({
+  const fetchRole = async () => {
+    const res = await getRole({
       row: pageSize,
       page: pageIndex * pageSize
     })
-    setPermission(res.data.rows);
-    setCountPermission(res.data.count);
+    setRole(res.data.rows);
+    setCountRole(res.data.count);
   }
 
   useEffect(() => {
-    fetchPermission();
+    fetchRole();
   }, [pageIndex, pageSize]);
 
   const handleDelete = async (id: number) => {
     try {
-      await deletePermission(id)
-      const res = await getPermission({
+      await deleteRole(id)
+      const res = await getRole({
         row: pageSize,
         page: pageIndex * pageSize
       })
-      setPermission(res.data.rows);
-      setCountPermission(res.data.count)
+      setRole(res.data.rows);
+      setCountRole(res.data.count)
     } catch (error) {
       console.error("Gagal menghapus data:", error)
     }
@@ -42,13 +42,13 @@ export default function Page() {
     <div className="flex flex-1 flex-col w-full px-6">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-          Master Access
-          <AddModal onSuccess={fetchPermission} />
+          Master Role
+          <AddModal onSuccess={fetchRole} />
         </CardTitle>
         <DataTable
-          data={permission}
-          refetch={fetchPermission}
-          count={countPermission}
+          data={role}
+          refetch={fetchRole}
+          count={countRole}
           pageIndex={pageIndex}
           pageSize={pageSize}
           setPageIndex={setPageIndex}
