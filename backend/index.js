dec4978d418bbfffe735b7bee2454c92e1b4229d
Fakhrizal Mustaqim <135151144+fakhrizalmus@users.noreply.github.com>
2025-12-02 21:28:34 +0700

    add auth

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,10 @@ const io = new Server(server, {
 });
 const path = require('path');
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000", // asal frontend
+  credentials: true,
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,6 +2,20 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
 });
+
+// Interceptor untuk cek session
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      if (typeof window !== "undefined") {
+        window.location.href = "/login"; // redirect ke login
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance
